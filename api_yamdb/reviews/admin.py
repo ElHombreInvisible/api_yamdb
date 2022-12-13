de14957f0620ee0db4fd14f04122a4f71232e148
Денис Суржиков <den.surzhikov@yandex.ru>
2022-12-13 18:32:47 +0300

    add admin settings

--- api_yamdb/reviews/admin.py
+++ api_yamdb/reviews/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Category, Genre, GenresOfTitle, Title
+
+
+class TitleAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'year', 'name', 'category')
+    search_fields = ('name',)
+    list_filter = ('year', 'category',)
+    empty_value_display = '-пусто-'
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug')
+
+class GenreAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug')
+
+admin.site.register(Title, TitleAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Genre, GenreAdmin)
+admin.site.register(GenresOfTitle)
