888b14208f51037c1f5761576017ff77c3fd16e9
Денис Суржиков <den.surzhikov@yandex.ru>
2022-12-13 16:21:08 +0300

    add models

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,3 +1,49 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+
+User = get_user_model()
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=256)
+    slug = models.SlugField(max_length=50,
+                            unique=True)
+    # description = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=256)
+    slug = models.SlugField(max_length=50,
+                            unique=True)
+    # description = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+
+class Title(models.Model):
+    name = models.CharField(max_length=256)
+    year = models.PositiveIntegerField()
+    description = models.TextField()
+    category = models.ManyToManyField(
+        Category, on_delete=models.CASCADE,
+        through='CategoriesOfTitle')
+    genre = models.ForeignKey(
+        Genre, on_delete=models.CASCADE,
+        blank=True, null=True)
+    # rating = 
+
+    def __str__(self):
+        return self.name
+
+
+class CategoriesOfTitle(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    title = models.ForeignKey(Title, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.title} категория:{self.category}'
